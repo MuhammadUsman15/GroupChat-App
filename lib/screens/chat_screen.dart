a78632002d1c0d162ac26e65ed5d232ba4d4d037
MuhammadUsman15 <80236188+MuhammadUsman15@users.noreply.github.com>
2023-08-17 22:56:00 +0500

    Update chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,8 +4,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+
 final _firestore = FirebaseFirestore.instance;
 late User loggedInUser;
+
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
